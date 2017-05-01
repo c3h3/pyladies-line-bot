6ac61928fa3079b62f2593b3fec9c767448bcb03
Chia-Chi Chang <c3h3.tw@gmail.com>
2017-05-01 10:38:54 +0800

    add message and event schema

--- todos_hello_mongodb.py
+++ todos_hello_mongodb.py
@@ -1,11 +1,19 @@
 def save_event(db, app_id, user_id, event):
     # TODO: create data from event object
     # data = {}
+    # hash_data = {"appId": app_id, "userId": user_id}
+    # data["uid"] = hashlib.sha256(json.dumps(hash_data,sort_keys=True).encode("utf-8")).hexdigest()
+    # data["event"] = 
+    # data["createAt"] = 
     # db.eventsLog.insert(data)
     pass
 
 def save_message(db, app_id, user_id, message):
     # TODO: create data from message object
     # data = {}
+    # hash_data = {"appId": app_id, "userId": user_id}
+    # data["uid"] = hashlib.sha256(json.dumps(hash_data,sort_keys=True).encode("utf-8")).hexdigest()
+    # data["message"] = 
+    # data["createAt"] = 
     # db.messages.insert(data)
     pass
